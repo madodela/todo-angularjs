59c100d157d4b15a797599b4aa110d39092728bd
Loli Delgado <madodela@gmail.com>
2015-06-14 23:09:25 -0500

    Starting with static application and backend

---
+++ README.md
@@ -0,0 +1,13 @@
+Task manager with AngularJS
+
+*Running back-end
+** Run npm install
+** Execute nodemon command
+Now you can see it in http://localhost:3000
+
+*Running front-end
+** Go to /web
+** Run bower install
+** Run npm install http-server
+** Execute http-server command
+You can see the app runing in http://localhost:8080/web/
---
+++ api/routes/tasks.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../dbConnection');
+
+router.param('idTask', function(req, res, next){
+	var idTask = req.params.idTask;
+	//The id must to be a number
+	req.idTask = isNaN(idTask) ? null : idTask;
+	next();
+});
+
+router.route('/')
+	.get(function(req, res){
+		db.all('SELECT * FROM Task', function(err, row) {
+			if(err){
+				res.status(400).end();	   
+			} else {
+				res.json({tasks:row});
+			}
+		});
+		
+	})
+	.post(function(req, res){		
+		db.run("INSERT INTO Task (title, description, overDue, status, priority) VALUES(?,?,?,?,?)",
+			[req.body.title, req.body.description, req.body.overDue,
+			req.body.status, req.body.priority], function(err){
+				if(err){
+					res.status(400).end();
+				} else {
+					res.send("Inserted");
+				}	
+			});
+	});
+
+router.route('/:idTask')
+	.get(function(req, res){
+		if(req.idTask){
+			db.get('SELECT * FROM Task WHERE idTask = ?', req.idTask, function(err, row) {
+				if(err){
+					res.status(400).end();	   
+				} else {
+					if(row){
+						res.json(row);
+					} else {
+						res.send(204).end();
+					}
+				}
+			});
+		} else {
+			res.status(400).end();
+		}
+		
+	})
+	.post(function(req, res){
+		if(req.idTask){
+			var keys = Object.keys(req.body);
+			db.run('UPDATE task SET '+keys[0]+' = ? WHERE idTask = ?', [req.body[keys[0]] ,req.idTask], function(err) {
+				if(err){
+					res.status(400).end();	
+				} else {
+					res.send("updated");
+				}
+			}); 
+		} else {
+			res.status(400).end();
+		}
+	})
+	.delete(function(req, res){
+		if(req.idTask){
+			db.run('DELETE FROM Task WHERE idTask = ?', req.idTask, function(err) {
+				if(err){
+					res.status(400).end();	
+				} else {
+					res.send("deleted");
+				}
+			});
+		} else {
+			res.status(400).end();
+		}
+	});
+
+module.exports = router;
