b5b0d274683e9729cbf09c9ddb09f1e203cad248
Loli Delgado <madodela@gmail.com>
2015-06-17 21:32:54 -0500

    Reorganized folder structure and initialized angular application"

---
+++ web/app/app.js
@@ -0,0 +1,11 @@
+// Create application module
+
+angular.module('TodoApp', []);
+
+// A little difference
+
+/*
+	angular.module('TodoApp', []); [Create]
+				VS
+	angular.module('TodoApp'); [Get]
+*/
